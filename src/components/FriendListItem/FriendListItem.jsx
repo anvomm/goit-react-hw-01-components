d7914a4d1d497bd4f2a2bf8193856ea075ea97a3
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-18 00:14:25 +0200

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,12 +1,7 @@
 export const FriendListItem = ({ avatar, name, isOnline }) => (
   <>
     <span className="status">{isOnline}</span>
-    <img
-      className="avatar"
-      src={avatar}
-      alt={name + ' ' + 'avatar'}
-      width="48"
-    />
+    <img className="avatar" src={avatar} alt={name} width="48" />
     <p className="name">{name}</p>
   </>
 );
