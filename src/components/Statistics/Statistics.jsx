1bade6583131a0139e3e3e415030ebb183c135bc
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-18 23:34:26 +0200

    Statistics stylization done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,22 @@
 import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
 
 export const Statistics = ({ title, stats }) => (
-  <section className="statistics">
-    {title && <h2 className="title">{title}</h2>}
-    <ul className="stat-list">
+  <section className={s.section}>
+    {title && <h2 className={s.title}>{title}</h2>}
+    <ul className={s.list}>
       {stats.map(({ id, label, percentage }) => (
-        <li key={id} className="item">
-          <span className="label">{label}</span>
-          <span className="percentage">{percentage}%</span>
+        <li
+          style={{
+            backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(
+              16
+            )}`,
+          }}
+          key={id}
+          className={s.item}
+        >
+          <span className={s.label}>{label}</span>
+          <span className={s.percentage}>{percentage}%</span>
         </li>
       ))}
     </ul>
